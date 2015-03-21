81e9fa3c34cd0a1131c1f5047a62fc5f8fb2188e
crobol <crobol@crobol.crobol>
2015-03-21 19:53:36 +0100

    Initial support for client-side encrypted links

--- migrations/init.sql
+++ migrations/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE `link` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `link` varchar(32) NOT NULL,
     `date_created` datetime NOT NULL,
-    `date_expired` datetime NOT NULL,
+    `date_expired` datetime NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `link` (`link`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
@@ -11,6 +11,16 @@ CREATE TABLE `redirect` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `link_id` int(11) NOT NULL,
     `redirect_uri` varchar(1024) NOT NULL,
+    `encrypted` boolean NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `link_fk1` FOREIGN KEY (`link_id`) REFERENCES `link` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `paste` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `link_id` int(11) NOT NULL,
+    `content` varchar(1024) NOT NULL, -- TODO: Change type of this
+    `encrypted` boolean NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `link_fk2` FOREIGN KEY (`link_id`) REFERENCES `link` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- server.go
+++ server.go
@@ -3,30 +3,59 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"github.com/go-martini/martini"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/martini-contrib/render"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
+
+type config struct {
+	SiteUrl  string
+	Port     int
+	Database dbConfig //`toml:"database"`
+}
+
+type dbConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Database string
+}
+
+type redirect struct {
+	Uri       string
+	Encrypted bool
+}
 
 var namespace = []rune("yuphjknm")
 
 func main() {
-	const root = "http://localhost:3000/"
-	const connectionString = "root@tcp(10.211.55.8:3306)/link"
+	var conf config
+	if _, err := toml.DecodeFile("config.toml", &conf); err != nil {
+		panic(err.Error())
+	}
+
+	root := conf.SiteUrl
+	connectionString := fmt.Sprintf("%s@tcp(%s:%d)/%s", conf.Database.User, conf.Database.Host, conf.Database.Port, conf.Database.Database)
+
+	fmt.Printf("Site URL: %s\n", root)
+	fmt.Printf("Connection string: %s\n", connectionString)
 
 	rand.Seed(time.Now().UTC().UnixNano())
 	m := martini.Classic()
+	m.Use(martini.Static("js", martini.StaticOptions{Prefix: "js"}))
 	m.Use(render.Renderer())
 
 	m.Get("/", func(renderer render.Render) {
-		db, err := sql.Open("mysql", connectionString)
-		if err != nil {
-			panic(err.Error())
-		}
-		defer db.Close()
+		//db, err := sql.Open("mysql", connectionString)
+		//if err != nil {
+		//panic(err.Error())
+		//}
+		//defer db.Close()
 
 		renderer.HTML(200, "index", nil)
 	})
@@ -39,9 +68,13 @@ func main() {
 		defer db.Close()
 
 		link := params["link"]
-		redirectUri := GetRedirectUriFromLink(db, link)
+		redirect := GetRedirectFromLink(db, link)
 
-		renderer.Redirect(redirectUri)
+		if redirect.Encrypted {
+			renderer.HTML(200, "encrypted", redirect.Uri)
+		} else {
+			renderer.Redirect(redirect.Uri)
+		}
 	})
 
 	m.Post("/", func(renderer render.Render, request *http.Request) {
@@ -58,14 +91,38 @@ func main() {
 		}
 
 		link := GenerateRandomLink(6)
-		InsertRedirectLink(db, link, url)
+		InsertRedirect(db, link, url, false)
 
 		linkUrl := root + link
 
 		renderer.HTML(200, "index", linkUrl)
 	})
 
-	m.Run()
+	m.Post("/api/link", func(r render.Render, request *http.Request) {
+		url := request.FormValue("url")
+		if url == "" {
+			panic("No parameter with key url provided")
+		}
+
+		encrypted, _ := strconv.ParseBool(request.FormValue("encrypted"))
+
+		fmt.Printf("POST recieved, url: %v", url)
+
+		db, err := sql.Open("mysql", connectionString)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		link := GenerateRandomLink(6)
+		InsertRedirect(db, link, url, encrypted)
+
+		linkUrl := root + link
+
+		r.JSON(200, linkUrl)
+	})
+
+	//m.Run()
+	m.RunOnAddr(fmt.Sprintf(":%d", conf.Port))
 }
 
 func GenerateRandomLink(length int) string {
@@ -99,6 +156,27 @@ func GetRedirectLinks(db *sql.DB) (string, string) {
 	return link, uri
 }
 
+func GetRedirectFromLink(db *sql.DB, link string) redirect {
+	stmtOut, err := db.Prepare("SELECT r.redirect_uri AS uri, r.encrypted AS encrypted FROM link l JOIN redirect r ON r.link_id = l.id WHERE l.link = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmtOut.Close()
+
+	var uri string
+	var encrypted bool
+	var redirect redirect
+	err = stmtOut.QueryRow(link).Scan(&uri, &encrypted)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	redirect.Uri = uri
+	redirect.Encrypted = encrypted
+
+	return redirect
+}
+
 func GetRedirectUriFromLink(db *sql.DB, link string) string {
 	stmtOut, err := db.Prepare("SELECT r.redirect_uri FROM link l JOIN redirect r ON r.link_id = l.id WHERE l.link = ?")
 	if err != nil {
@@ -115,7 +193,7 @@ func GetRedirectUriFromLink(db *sql.DB, link string) string {
 	return redirectUrl
 }
 
-func InsertRedirect(db *sql.DB, link string, uri string) bool {
+func InsertRedirect(db *sql.DB, link string, uri string, encrypted bool) bool {
 	linkIns, err := db.Prepare("INSERT INTO link (link, date_created) VALUES (?, NOW())")
 	if err != nil {
 		panic(err.Error())
@@ -132,13 +210,13 @@ func InsertRedirect(db *sql.DB, link string, uri string) bool {
 		panic(err.Error())
 	}
 
-	redirectIns, err := db.Prepare("INSERT INTO redirect (link_id, redirect_uri) VALUES (?, ?)")
+	redirectIns, err := db.Prepare("INSERT INTO redirect (link_id, redirect_uri, encrypted) VALUES (?, ?, ?)")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer redirectIns.Close()
 
-	_, err = redirectIns.Exec(linkId, uri)
+	_, err = redirectIns.Exec(linkId, uri, encrypted)
 	if err != nil {
 		panic(err.Error())
 	}
